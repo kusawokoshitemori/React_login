95adfbc51b8c1e4dab80218d88efdda808ea1de5
草をこして森 <kyou0317kyou0317@gmail.com>
2024-10-11 23:20:27 +0900

    Youtubeの解説は終わり。ここから自力で

--- react-hook-form-yt/src/App.tsx
+++ react-hook-form-yt/src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
+import { validationSchema } from "./utils/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface LoginForm {
   name: string;
@@ -12,7 +14,10 @@ function App() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<LoginForm>({ mode: "onChange" });
+  } = useForm<LoginForm>({
+    mode: "onChange",
+    resolver: zodResolver(validationSchema),
+  });
 
   const onSubmit = (data: LoginForm) => {
     console.log(data);
@@ -23,31 +28,13 @@ function App() {
       <h1>Login Form</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="名前">名前</label>
-        <input
-          id="name"
-          type="text"
-          {...register("name", {
-            required: "名前は必須です",
-            minLength: { value: 4, message: "4文字以上で入力してください" },
-          })}
-        />
+        <input id="name" type="text" {...register("name")} />
         <p>{errors.name?.message as React.ReactNode}</p>
         <label htmlFor="メールアドレス">メールアドレス</label>
-        <input
-          id="email"
-          type="email"
-          {...register("email", { required: "Emailは必須です" })}
-        />
+        <input id="email" type="email" {...register("email")} />
         <p>{errors.email?.message as React.ReactNode}</p>
         <label htmlFor="パスワード">パスワード</label>
-        <input
-          id="password"
-          type="password"
-          {...register("password", {
-            required: "パスワードは必須です",
-            minLength: { value: 6, message: "6文字以上で入力してください" },
-          })}
-        />
+        <input id="password" type="password" {...register("password")} />
         <p>{errors.password?.message as React.ReactNode}</p>
 
         <button type="submit">送信</button>
