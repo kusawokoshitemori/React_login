8d7bb6faf10c579e0a7a33bcfb2e9b6c1350d656
草をこして森 <kyou0317kyou0317@gmail.com>
2024-10-11 20:46:11 +0900

    チュートリアル見てやってみた

--- react-hook-form-yt/src/App.tsx
+++ react-hook-form-yt/src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useForm } from "react-hook-form";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// フォームデータの型定義
+interface FormData {
+  example: string;
 }
 
-export default App
+function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = (data: FormData) => console.log(data);
+
+  console.log(watch("example"));
+
+  return (
+    <div className="form-container">
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="名前">名前</label>
+        <input id="name" type="text" {...register("example")} />
+        <label htmlFor="メールアドレス">メールアドレス</label>
+        <input id="email" type="email" {...register("example")} />
+        <label htmlFor="パスワード">パスワード</label>
+        <input id="password" type="password" {...register("example")} />
+
+        {/* エラーメッセージの表示 */}
+        {errors.example?.message && <p>{String(errors.example.message)}</p>}
+
+        <button type="submit">送信</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
