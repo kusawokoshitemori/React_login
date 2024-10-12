124738d3baab1033dd301e8a6c16c93ab61a026e
草をこして森 <kyou0317kyou0317@gmail.com>
2024-10-12 20:50:02 +0900

    やっと動くようになった~~~

--- loginreact/src/app/page.tsx
+++ loginreact/src/app/page.tsx
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { useForm } from "react-hook-form";
+import { validationSchema } from "../app/utils/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+//import "./index.css";
+
+interface LoginForm {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const App = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+    resolver: zodResolver(validationSchema),
+  });
+
+  const onSubmit = async (data: LoginForm) => {
+    try {
+      const response = await axios.post("http://localhost:3000/api/login", {
+        email: data.email,
+        password: data.password,
+      });
+      console.log(response.data.message); // ログイン成功メッセージを表示
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(error.response?.data.message); // エラーメッセージを表示
+      } else {
+        console.error("Unexpected error:", error);
+      }
+    }
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="form-container">
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="名前">名前</label>
+        <input id="name" type="text" {...register("name")} />
+        <p>{errors.name?.message as React.ReactNode}</p>
+        <label htmlFor="メールアドレス">メールアドレス</label>
+        <input id="email" type="email" {...register("email")} />
+        <p>{errors.email?.message as React.ReactNode}</p>
+        <label htmlFor="パスワード">パスワード</label>
+        <input id="password" type="password" {...register("password")} />
+        <p>{errors.password?.message as React.ReactNode}</p>
+        <button type="submit">送信</button>
+      </form>
     </div>
   );
-}
+};
+
+export default App;
