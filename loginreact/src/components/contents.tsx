31ff091c5befc6565eaf71b8420b82422c763307
草をこして森 <kyou0317kyou0317@gmail.com>
2024-10-19 11:22:00 +0900

    いいねコメント詳細追加(レイアウト)

--- loginreact/src/components/contents.tsx
+++ loginreact/src/components/contents.tsx
@@ -15,9 +15,40 @@ const Contents = () => {
         />
         <p className="whitespace-nowrap text-lg">ここに名前入る</p>
       </div>
-      <p className="text-2xl flex items-center justify-center mt-2">
+      {/* 画面の大きさで文字の大きさ変えてるから注意 */}
+      <p className="text-2xl sm:text-4xl flex items-center justify-center mt-2">
         ここにことわざ
       </p>
+      <div className="w-5/6 flex justify-between items-center mx-auto">
+        <div className="flex items-center">
+          <Image
+            src="/heart_white.png"
+            alt="ハートの画像"
+            width={50}
+            height={50}
+            className="rounded-full mr-4 ml-1 m-1"
+          />
+          <p text-lg>1</p>
+        </div>
+        <div className="flex items-center">
+          {" "}
+          <Image
+            src="/comment_white.png"
+            alt="コメントの画像"
+            width={50}
+            height={50}
+            className="rounded-full mr-4 ml-1 m-1"
+          />
+          <p>0</p>
+        </div>
+        <Image
+          src="/right_arrow.png"
+          alt="▷の画像"
+          width={50}
+          height={50}
+          className="rounded-full mr-4 ml-1 m-1"
+        />
+      </div>
     </div>
   );
 };
