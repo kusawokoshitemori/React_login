dc099a3ba15c2b5a3df548976fc50a9615358a94
草をこして森 <kyou0317kyou0317@gmail.com>
2024-12-10 23:53:02 +0900

    イラストたち追加

--- loginreact/src/components/FirstPage/Main/PinkContainer.tsx
+++ loginreact/src/components/FirstPage/Main/PinkContainer.tsx
@@ -3,15 +3,55 @@ import Image from "next/image";
 
 const PinkContainer = () => {
   return (
-    <div className="font-bold text-3xl py-12">
-      <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center">
+      {/* ロゴ本体 */}
+      <div className="z-10 ">
         <Image
           src="/proverb_icon.png"
-          alt="Sample Image"
-          layout="intrinsic" // 元のアスペクト比を保持
-          width={800} // 幅を指定
-          height={200} // 必要に応じて高さも指定
+          alt="ことわざの輪ロゴ"
+          width={768}
+          height={768}
         />
+      </div>
+
+      {/* イラストの配置 */}
+      <div className="absolute top-0 left-0 transform translate-x-[-10%] translate-y-[-10%]">
+        <Image
+          src="/proverb/proverb1.png"
+          alt="ことわざ1"
+          width={48}
+          height={48}
+        />
+      </div>
+      <div className="absolute top-0 right-0 transform translate-x-[50%] translate-y-[-50%]">
+        <Image
+          src="/proverb/proverb2.png"
+          alt="ことわざ2"
+          width={48}
+          height={48}
+        />
+      </div>
+      <div className="absolute bottom-0 left-0 transform translate-x-[-50%] translate-y-[50%]">
+        <Image
+          src="/proverb/proverb3.png"
+          alt="ことわざ3"
+          width={48}
+          height={48}
+        />
+      </div>
+      <div className="absolute bottom-0 right-0 transform translate-x-[50%] translate-y-[50%]">
+        <Image
+          src="/proverb/proverb4.png"
+          alt="ことわざ4"
+          width={48}
+          height={48}
+        />
+      </div>
+      <div className="absolute top-1/2 left-0 transform translate-x-[-50%] translate-y-[-50%]">
+        <Image src="/proverb/flower.png" alt="花1" width={48} height={48} />
+      </div>
+      <div className="absolute top-1/2 right-0 transform translate-x-[50%] translate-y-[-50%]">
+        <Image src="/proverb/flower.png" alt="花2" width={48} height={48} />
       </div>
     </div>
   );
