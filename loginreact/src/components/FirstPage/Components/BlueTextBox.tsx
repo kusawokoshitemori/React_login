026aa5e340fb7fad79b82e1707fb082c9de77b15
草をこして森 <kyou0317kyou0317@gmail.com>
2024-12-12 10:51:20 +0900

    Blue要素完成(レスポンシブ未対応)

--- loginreact/src/components/FirstPage/Components/BlueTextBox.tsx
+++ loginreact/src/components/FirstPage/Components/BlueTextBox.tsx
@@ -6,7 +6,7 @@ interface TextBoxProps {
 
 const BlueTextBox: React.FC<TextBoxProps> = ({ text }) => {
   return (
-    <div className="inline-block bg-gradient-to-r from-blue-400 to-blue-600 border-2 border-blue-800 rounded-lg shadow-xl p-4 text-center transform transition-transform duration-300 hover:scale-105">
+    <div className="inline-block bg-gradient-to-r from-blue-400 to-blue-600 border-2 border-blue-800 rounded-lg shadow-xl p-4 text-center transform transition-transform duration-300 hover:scale-125">
       <p className="text-white font-semibold text-lg tracking-wide">{text}</p>
     </div>
   );
--- loginreact/src/components/FirstPage/Main/BlueContainer.tsx
+++ loginreact/src/components/FirstPage/Main/BlueContainer.tsx
@@ -3,8 +3,20 @@ import Image from "next/image";
 import BlueTextBox from "../Components/BlueTextBox";
 const BlueContainer = () => {
   return (
-    <div className="font-bold text-3xl pb-60">
-      <div className="flex">
+    <div className="flex flex-col">
+      <div className="flex justify-center items-center">
+        <Image
+          src="/textSubmit.png"
+          alt="ことわざを発信しよう"
+          width={512}
+          height={512}
+        />
+      </div>
+
+      <div
+        className="flex justify-between m-auto pb-96"
+        style={{ width: "90%" }}
+      >
         <Image
           src="/postingPhoto.png"
           alt="アプリ画面の様子"
@@ -12,7 +24,10 @@ const BlueContainer = () => {
           height={768}
         />
         <div className="flex flex-col justify-center items-center">
-          <BlueTextBox text="あなたのことわざを簡単シェア" />
+          <div className="mb-4">
+            <BlueTextBox text="あなたのことわざを簡単シェア" />
+          </div>
+
           <BlueTextBox text="ことわざ　ｘ　SNS" />
         </div>
       </div>
