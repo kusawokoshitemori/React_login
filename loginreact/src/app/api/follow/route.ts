4abd919b1d6301e800dc6d8d02f4c0c072a9c6c7
草をこして森 <kyou0317kyou0317@gmail.com>
2024-12-17 17:15:27 +0900

    request.urlを排除した

--- loginreact/src/app/api/follow/route.ts
+++ loginreact/src/app/api/follow/route.ts
@@ -2,32 +2,59 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/services/supabaseClient";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const userId = searchParams.get("userId");
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get("userId");
 
-  //フォローの数を返す
-  const { count: followCount, error: followError } = await supabase
-    .from("follows")
-    .select("*", { count: "exact" })
-    .eq("follower_id", userId);
+    if (!userId) {
+      return NextResponse.json(
+        { error: "userIdが指定されていません" },
+        { status: 400 }
+      );
+    }
 
-  //フォロワーの数を返す
-  const { count: followersCount, error: followersError } = await supabase
-    .from("follows")
-    .select("*", { count: "exact" }) // フォロワー数をカウント
-    .eq("followee_id", userId);
+    //フォローの数を返す
+    const { count: followCount, error: followError } = await supabase
+      .from("follows")
+      .select("*", { count: "exact" })
+      .eq("follower_id", userId);
 
-  //エラーチェック
-  if (followersError || followError) {
+    if (followError) {
+      console.error("Supabaseのエラーが発生しました", followError.message);
+      return NextResponse.json(
+        { error: "フォロー数の取得中にエラーが発生しました" },
+        { status: 500 }
+      );
+    }
+
+    //フォロワーの数を返す
+    const { count: followersCount, error: followersError } = await supabase
+      .from("follows")
+      .select("*", { count: "exact" }) // フォロワー数をカウント
+      .eq("followee_id", userId);
+
+    if (followersError) {
+      console.error("Supabaseのエラーが発生しました", followersError.message);
+      return NextResponse.json(
+        { error: "フォロワー数の取得中にエラーが発生しました" },
+        { status: 500 }
+      );
+    }
+
+    // ここでデータを送信する
+    if (followCount && followersCount) {
+      return NextResponse.json({
+        success: true,
+        followCount,
+        followersCount,
+      });
+    }
+  } catch (error) {
+    console.error("予期しないエラーが発生しました", error);
     return NextResponse.json(
-      {
-        success: false,
-        message: followersError?.message || followError?.message,
-      },
-      { status: 400 }
+      { error: "予期しないエラーが発生しました" },
+      { status: 500 }
     );
   }
-
-  return NextResponse.json({ userId, followersCount, followCount });
 }
